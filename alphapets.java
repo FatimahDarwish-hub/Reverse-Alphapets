8862d13649728cf76f58c940bae3be9d2f8cdda7
Fatimah Alshaikhi <51248702+FatimahDarwish-hub@users.noreply.github.com>
2022-03-06 21:33:40 +0300

    Create alphapets.java

---
+++ alphapets.java
@@ -0,0 +1,12 @@
+public class Alphapets {
+
+
+    public static void main(String[] args) {
+        System.out.println("Reverse 30-1");
+        for ( int ch = 30; ch>=1; ch--)
+        {
+            System.out.println(ch);
+        }
+    }
+    
+}
