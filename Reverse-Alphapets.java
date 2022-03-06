c0a5172bbc9f0669d8c312890506dc63bb90ef5f
Fatimah Alshaikhi <51248702+FatimahDarwish-hub@users.noreply.github.com>
2022-03-06 21:32:44 +0300

    Create Reverse-Alphapets.java

---
+++ Reverse-Alphapets.java
@@ -0,0 +1,12 @@
+public class Alphapets {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Reverse Alphapets");
+        for ( char ch = 'z'; ch>='a'; ch--)
+        {
+            System.out.println(ch);
+        }
+    }
+    
+}
